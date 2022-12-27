a793d25885f0941a653293dda0c4e5297a43d9b6
unknown <gabrielfdias082@gmail.com>
2022-12-26 21:41:22 -0300

    Primeiro commit projeto to do it

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const btn = document.getElementById('')
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', function(){
+    let input = document.getElementById('input')
+    let task = document.querySelector('.div-tarefas')  
+    let div = document.createElement('div') // <==  .createElement() para criar nova div dentro da div de tasks (div-tarefas)
+    div.innerText = input.value // <== inserindo valor do input - (.value())
+    div.classList = 'task' // <== adicionando classe pre-definida a div criada
+    task.appendChild(div) // <== adicionando div criada na div principal como seu filho
+})
