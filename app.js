50721a1e49e99c57fa879ecabedb9c98eefac71a
unknown <gabrielfdias082@gmail.com>
2022-12-27 21:46:43 -0300

    att código limpo

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const btn = document.querySelector('.btn')
 btn.addEventListener('click', function(){
     const input = document.getElementById('input')
     const task = document.querySelector('.div-tarefas')  
-    const div = document.createElement('div')
-    const createImgCheck = document.createElement('img')
-    const createImgTrash = document.createElement('img')
+    
+    function criarDivTask(){
+        const div = document.createElement('div')
+        const createImgCheck = document.createElement('img')
+        const createImgTrash = document.createElement('img')
 
-        function criarDivTask(){
             createImgTrash.src = 'icons/trash.png'
             createImgCheck.src = 'icons/check.png'
 
@@ -20,24 +21,27 @@ btn.addEventListener('click', function(){
             div.appendChild(createImgCheck)
             div.appendChild(createImgTrash)
             task.appendChild(div)
+
+
+                function interatividadeTask (){
+            createImgCheck.addEventListener('click', function(){
+                if(div.style.textDecoration != 'line-through'){
+                    div.style.textDecoration = 'line-through'
+                } else {
+                    div.style.textDecoration = 'solid'
+                }
+            })
+        
+            createImgTrash.addEventListener('click', function(){
+                if (div.style.display != 'none'){
+                    div.style.display = 'none'
+                }
+            })
+        }
+
+    interatividadeTask()
     }
 
-        function interatividadeTask (){
-        createImgCheck.addEventListener('click', function(){
-            if(div.style.textDecoration != 'line-through'){
-                div.style.textDecoration = 'line-through'
-            } else {
-                div.style.textDecoration = 'solid'
-            }
-        })
-    
-        createImgTrash.addEventListener('click', function(){
-            if (div.style.display != 'none'){
-                div.style.display = 'none'
-            }
-        })
-    }
 
 criarDivTask()
-interatividadeTask()
 })
