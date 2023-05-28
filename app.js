ef7529913aa8cfe20edbd0ddf02c8d26edcee5d2
unknown <gabrielfdias082@gmail.com>
2023-05-28 00:43:13 -0300

    nova versão de código

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
 const btn = document.querySelector('.btn')
+const input = document.getElementById('input')
 
-btn.addEventListener('click', function(){
-    const input = document.getElementById('input')
-    const task = document.querySelector('.div-tarefas')  
+function criarElemento(){
+
+const divPai = document.querySelector('.div-tarefas')
+
+const valorInput = input.value
+
+let divTask = document.createElement('div')
+    divTask.classList.add('task')
+    divPai.appendChild(divTask)
+
+let paragrafoTask = document.createElement('p')
+    divTask.appendChild(paragrafoTask)
+    paragrafoTask.innerText = valorInput
+
+let imgCheckedButton = document.createElement('img')
+    divTask.appendChild(imgCheckedButton)
+    imgCheckedButton.classList.add('img-task')
+    imgCheckedButton.src = 'icons/check.png'
     
-    function criarDivTask(){
-        const div = document.createElement('div')
-        const createImgCheck = document.createElement('img')
-        const createImgTrash = document.createElement('img')
+let imgTrashButton = document.createElement('img')
+    divTask.appendChild(imgTrashButton)
+    imgTrashButton.classList.add('img-task-trash')
+    imgTrashButton.src = 'icons/trash.png'
 
-            createImgTrash.src = 'icons/trash.png'
-            createImgCheck.src = 'icons/check.png'
+    
+    imgTrashButton.addEventListener('click', function(){
+        removerElemento(divPai, divTask)
+    })
+    
+    imgCheckedButton.addEventListener('click', function(){
+        if(paragrafoTask.style.textDecoration != 'line-through'){
+            paragrafoTask.style.textDecoration = 'line-through'
+            paragrafoTask.style.color = 'grey'
+            paragrafoTask.style.fontStyle = 'italic'
 
-            createImgTrash.classList = 'img-task'
-            createImgCheck.classList = 'img-task'
-            div.classList = 'task'
+        } else {
+            paragrafoTask.style.textDecoration = 'solid'
+            paragrafoTask.style.color = 'black'
+            paragrafoTask.style.fontStyle = 'normal'
 
-            div.innerHTML = '<p>' + input.value + '</p>'
-
-            div.appendChild(createImgCheck)
-            div.appendChild(createImgTrash)
-            task.appendChild(div)
-
-
-                function interatividadeTask (){
-            createImgCheck.addEventListener('click', function(){
-                if(div.style.textDecoration != 'line-through'){
-                    div.style.textDecoration = 'line-through'
-                } else {
-                    div.style.textDecoration = 'solid'
-                }
-            })
-        
-            createImgTrash.addEventListener('click', function(){
-                if (div.style.display != 'none'){
-                    div.style.display = 'none'
-                }
-            })
         }
 
-    interatividadeTask()
-    }
+    })
+}
+
+function removerElemento(elementoPai, elementoFilho){
+    elementoPai.removeChild(elementoFilho)
+}
 
 
-criarDivTask()
+
+
+btn.addEventListener('click', function(){
+
+
+criarElemento()
+
+    
+
 })
