7e48e33206353a05652e1017ff789599a3dd4837
unknown <gabrielfdias082@gmail.com>
2023-05-29 20:37:45 -0300

    re-organização das function

--- app.js
+++ app.js
@@ -1,62 +1,51 @@
-const btn = document.querySelector('.btn')
-const input = document.getElementById('input')
-const divPai = document.querySelector('.div-tarefas')
+const btn = document.querySelector('.btn');
+const input = document.getElementById('input');
+const divPai = document.querySelector('.div-tarefas');
 
+btn.addEventListener('click', function() {
+  const divTask = document.createElement('div');
+  divTask.classList.add('task');
+  divPai.appendChild(divTask);
 
+  criacaoParagrafo(divTask);
+});
 
-btn.addEventListener('click', function(){
+function criacaoParagrafo(divTask) {
+  const valorInput = input.value;
+  let paragrafoTask = document.createElement('p');
+  divTask.appendChild(paragrafoTask);
+  paragrafoTask.innerText = valorInput;
 
+  botaoCheck(divTask, paragrafoTask);
+  botaoDelete(divTask);
+}
 
-    const divTask = document.createElement('div')
-        divTask.classList.add('task')
-        divPai.appendChild(divTask)
+function botaoCheck(divTask, paragrafoTask) {
+  let imgCheckedButton = document.createElement('img');
+  divTask.appendChild(imgCheckedButton);
+  imgCheckedButton.classList.add('img-task');
+  imgCheckedButton.src = 'icons/check.png';
 
-        function criacaoParagrafo(){
-            const valorInput = input.value
-            
-                let paragrafoTask = document.createElement('p')
-                    divTask.appendChild(paragrafoTask)
-                    paragrafoTask.innerText = valorInput
+  imgCheckedButton.addEventListener('click', function() {
+    if (paragrafoTask.style.textDecoration !== 'line-through') {
+      paragrafoTask.style.textDecoration = 'line-through';
+      paragrafoTask.style.color = 'grey';
+      paragrafoTask.style.fontStyle = 'italic';
+    } else {
+      paragrafoTask.style.textDecoration = 'solid';
+      paragrafoTask.style.color = 'black';
+      paragrafoTask.style.fontStyle = 'normal';
+    }
+  });
+}
 
-                    function botaoCheck(){
-                        let imgCheckedButton = document.createElement('img')
-                        divTask.appendChild(imgCheckedButton)
-                        imgCheckedButton.classList.add('img-task')
-                        imgCheckedButton.src = 'icons/check.png' 
-                        
-                        imgCheckedButton.addEventListener('click', function(){
-                            if(paragrafoTask.style.textDecoration != 'line-through'){
-                                paragrafoTask.style.textDecoration = 'line-through'
-                                paragrafoTask.style.color = 'grey'
-                                paragrafoTask.style.fontStyle = 'italic'
-                    
-                            } else {
-                                paragrafoTask.style.textDecoration = 'solid'
-                                paragrafoTask.style.color = 'black'
-                                paragrafoTask.style.fontStyle = 'normal'
-                    
-                            }
-                    
-                        })
-                    }
-                    botaoCheck()
-                    
-                    function botaoDelete(){
-                    let imgTrashButton = document.createElement('img')
-                        divTask.appendChild(imgTrashButton)
-                        imgTrashButton.classList.add('img-task-trash')
-                        imgTrashButton.src = 'icons/trash.png'
-            
-                        imgTrashButton.addEventListener('click', function removerElemento(){
-                            divPai.removeChild(divTask)
-                        })
-                    }
-                        botaoDelete()
-            }
-            criacaoParagrafo()
+function botaoDelete(divTask) {
+  let imgTrashButton = document.createElement('img');
+  divTask.appendChild(imgTrashButton);
+  imgTrashButton.classList.add('img-task-trash');
+  imgTrashButton.src = 'icons/trash.png';
 
-
-
-    
-
-})
+  imgTrashButton.addEventListener('click', function removerElemento() {
+    divPai.removeChild(divTask);
+  });
+}
